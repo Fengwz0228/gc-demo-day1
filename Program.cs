74e79adf6d5a30ec0bba76d6d6287e8754e2d9bb
kevin.feng <Kevin.Feng@grapecity.com>
2022-06-15 17:56:20 +0800

    fix sonarlint

--- Program.cs
+++ Program.cs
@@ -1,44 +1,52 @@
 ﻿using System.Text;
 
-namespace Fengweizhe
+namespace gc_demo_day1
 {
-    class Sort
+    public static class MySort
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("请输入要排序的数字组, 用逗号隔开:");
-            String line = Console.ReadLine();
-            String[] arr = line.Split(",");
-            List<int> list = new List<int>();
-            for (int i = 0; i < arr.Length; i++)
+            var line = Console.ReadLine();
+            var arr = line?.Split(",");
+            List<int> list = new();
+            for (var i = 0; i < arr?.Length; i++)
             {
-                if (arr[i].Trim() == "") continue;
+                if (arr[i].Trim() == "")
+                {
+                    continue;
+                }
+
                 try
                 {
                     list.Add(int.Parse(arr[i]));
                 }
-                catch (Exception)
+                catch (Exception e)
                 {
-                    continue;
+                    Console.WriteLine(e);
                 }
             }
-            sort(list, 0, list.Count - 1);
+
+            QuickSort(list, 0, list.Count - 1);
             Console.WriteLine("快速排序后的结果如下:");
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new();
             foreach (int i in list)
             {
-                if (sb.Length == 0)
-                    sb.Append(i.ToString());
-                else
-                    sb.Append(",").Append(i.ToString());
+                if (sb.Length != 0)
+                {
+                    sb.Append(',');
+                }
+
+                sb.Append(i);
             }
+
             Console.WriteLine(sb.ToString());
         }
 
-        public static void sort(List<int> list, int min, int max)
+        private static void QuickSort(List<int> list, int min, int max)
         {
             if (list.Count == 0) return;
-            int key = list[min];// 取第一个值作为基准值
+            int key = list[min]; // 取第一个值作为基准值
             int start = min; // 从左向右遍历的指针
             int end = max; // 从右向左遍历的指针 
             // 当左右指针不相遇
@@ -57,11 +65,13 @@ namespace Fengweizhe
                     list[end] = list[start];
                     list[start] = temp;
                 }
+
                 // 从左向右找，找到大于基准值的
                 while (end > start && list[start] <= key)
                 {
                     start++;
                 }
+
                 if (key < list[start])
                 {
                     int temp = list[start];
@@ -69,16 +79,18 @@ namespace Fengweizhe
                     list[end] = temp;
                 }
             }
+
             // 此时基数的左边全小于基数， 基数的右边全大于基数
             // 排序基数左边
             if (start > min)
             {
-                sort(list, min, start - 1);
+                QuickSort(list, min, start - 1);
             }
+
             // 排序基数右边
             if (end < max)
             {
-                sort(list, end + 1, max);
+                QuickSort(list, end + 1, max);
             }
         }
     }
